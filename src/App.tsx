80b74322552111d055ca58ef46aee6b9f90d3845
AmilMasic <amil.masic@gmail.com>
2025-04-02 22:22:08 -0400

    add reducer and initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useReducer } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { State, Action } from "./Types/gamestate";
+function getInitialState(): State {
+  return { phase: "pre-game" };
+}
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "start-game": {
+      console.log("game started");
+      break;
+    }
+    case "update-guess": {
+      console.log("updated guess");
+      break;
+    }
+  }
+
+  return state;
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, null, getInitialState);
   return (
     <div className="App">
       <header className="App-header">
