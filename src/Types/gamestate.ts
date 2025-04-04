6c1d73b0aa80f5257ea11d3ba06c999300131451
AmilMasic <amil.masic@gmail.com>
2025-04-04 13:45:52 -0400

    update types to handle wordPack

--- src/Types/gamestate.ts
+++ src/Types/gamestate.ts
@@ -1,17 +1,21 @@
 export type State =
   | {
       phase: "pre-game";
+      wordPack: readonly string[] | null;
     }
   | {
       phase: "in-game";
       goal: string;
       guess: string;
+      wordPack: readonly string[];
     }
   | {
       phase: "post-game";
       goal: string;
+      wordPack: readonly string[];
     };
 
 export type Action =
   | { type: "start-game" }
+  | { type: "load-data"; wordPack: readonly string[] }
   | { type: "update-guess"; newGuess: string };
